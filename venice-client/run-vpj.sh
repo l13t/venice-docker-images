17c3c3d329485f28e9bc2e713a10dd6ee3858027
Dmytro Prokhorenkov <liet@liet.kiev.ua>
2024-02-14 15:07:03 +0100

    Added AWS jars and modified script

--- venice-client/run-vpj.sh
+++ venice-client/run-vpj.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-hadoop_jars="/opt/venice/bin/hadoop-mapreduce-client-core.jar:/opt/venice/bin/hadoop-mapreduce-client-common.jar:/opt/venice/bin/hadoop-common.jar"
-vpj_jar="/opt/venice/bin/venice-push-job-all.jar" 
+hadoop_jars="/opt/venice/bin/hadoop-mapreduce-client-core.jar:/opt/venice/bin/hadoop-mapreduce-client-common.jar:/opt/venice/bin/hadoop-common.jar:/opt/venice/bin/hadoop-aws.jar:/opt/venice/bin/aws-java-sdk-bundle.ja"
+vpj_jar="/opt/venice/bin/venice-push-job-all.jar"
 
 java -cp "$hadoop_jars:$vpj_jar" com.linkedin.venice.hadoop.VenicePushJob $1
